49904dad6096d944ab01ec8e6ba7a250ef72ac13
ryan <ryan@binarydad.com>
2024-03-20 19:08:43 +0000

    update build scripts for aot

--- Aot/build.ps1
+++ Aot/build.ps1
@@ -1,3 +1,4 @@
 dotnet clean
 
+# create a windows x64 native build
 dotnet publish -r win-x64 -c Release
---
+++ Aot/build.sh
@@ -0,0 +1,4 @@
+dotnet clean
+
+# create a linux x64 native build
+dotnet publish -r linux-x64 -c Release
