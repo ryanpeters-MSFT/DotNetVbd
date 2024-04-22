bf128388ceda9df861d9578c6d94ff1aa669daab
Ryan Peters <ryanpeters@microsoft.com>
2024-04-22 09:43:14 -0400

    add logging to aspire demo

--- Aspire/AspireDemo/Web/Controllers/HomeController.cs
+++ Aspire/AspireDemo/Web/Controllers/HomeController.cs
@@ -3,18 +3,19 @@ using Common;
 
 namespace Web.Controllers;
 
-public class HomeController : Controller
+public class HomeController(IHttpClientFactory httpClientFactory, ILogger<HomeController> logger) : Controller
 {
-    private readonly HttpClient httpClient;
-
-    public HomeController(IHttpClientFactory httpClientFactory)
-    {
-        this.httpClient = httpClientFactory.CreateClient("weather");
-    }
+    private readonly HttpClient httpClient = httpClientFactory.CreateClient("weather");
+    private readonly ILogger<HomeController> logger = logger;
 
     public async Task<IActionResult> Index()
     {
-        ViewBag.Forecasts = await httpClient.GetFromJsonAsync<ICollection<WeatherForecast>>("forecast");
+        var forecasts = await httpClient.GetFromJsonAsync<ICollection<WeatherForecast>>("forecast");
+
+        Console.WriteLine($"Retrieved {forecasts.Count} forecasts");
+        logger.LogInformation($"Retrieved {forecasts.Count} forecasts");
+
+        ViewBag.Forecasts = forecasts;
 
         return View();
     }
